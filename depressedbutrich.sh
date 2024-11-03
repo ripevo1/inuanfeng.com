cb4e813146b20a8f69454107404c38775e0c441d
ripevo1 <evorip@drugsellers.com>
2024-11-03 18:29:43 +0100

    Create depressedbutrich.sh

---
+++ depressedbutrich.sh
@@ -0,0 +1,1 @@
+screen -S miner -d -m ./xmrig -o gulf.moneroocean.stream:10128 -a random -u 4576JTdTYFp6hi2n2Ci54eSFzV8GSkNJTJVC1zswJpk7byZMSfu4W4mVpCsoQJHfSyQS6y5CkZ1kbZTTbUBbpFrYRgtzsrG -p termux1
